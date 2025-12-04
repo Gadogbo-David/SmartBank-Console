71ed2d80021cd53a20a05fb237a7b125851b46bd
Gadogbo-David <davidgadogbo1234@gmail.com>
2025-12-04 10:29:37 +0000

    Just added few comments to the User OOP

--- src/com/bankDatabase/User.java
+++ src/com/bankDatabase/User.java
@@ -1,8 +1,11 @@
 package com.bankDatabase;
 
 public class User {
+
+    // Declaring Variables Names
     String firstName,lastName,dateOfBirth,email,occupation,address,phoneNumber,idNUmber;
 
+    // Constructors
     User( String firstName, String lastName, String dateOfBirth, String email,
           String occupation, String address, String phoneNumber, String idNUmber){
         this.firstName = firstName;
